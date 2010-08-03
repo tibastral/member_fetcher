f540e95b113a8a2ef1f0c67bd1641e39b36c86ec
Thibaut Assus <thibaut.assus@gmail.com>
2010-08-03 19:31:11 +0200

    member_fetcher now better, and less bugguy

--- lib/member_fetcher.rb
+++ lib/member_fetcher.rb
@@ -6,9 +6,12 @@ module MemberFetcher
   private
   def member_fetcher
     if params[:id]
-      model_name = self.class.to_s.gsub("Controller","").singularize
+      model_name = self.class.to_s.gsub("Controller","").gsub("Admin::", "").singularize
       object_name = model_name.downcase
-      instance_variable_set("@#{object_name}", eval(model_name).find(params[:id]))
+      begin
+        instance_variable_set("@#{object_name}", eval(model_name).find(params[:id]))
+      rescue
+      end
     end
   end
 end
